68692397d284bef15b749e2ee00362d9b5b33d5d
Alejandro Pardo <alejandropardo@Alejandros-MacBook-Pro.local>
2016-11-07 22:53:59 +0100

    First commit"

--- Shoe Converter/ViewController.swift
+++ Shoe Converter/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertMensShoeSizeButtonPressed(_ sender: UIButton) {
+        let sizeFromTextField = Int(mensShoeSizeTextField.text!)!
+        let conversionConstant = 30
+        let mensConvertedSize = sizeFromTextField + conversionConstant
+        
+        mensConvertedShoeSizeLabel.text = "\(mensConvertedSize) in European Shoe Size"
+    }
+    
+    @IBAction func convertWomensShoeSizeButtonPressed(_ sender: UIButton) {
+        let sizeFromTextField = Double(womensShoeSizeTextField.text!)!
+        let conversionConstant = 30.5
+        let womensConvertedSize = sizeFromTextField + conversionConstant
+        
+        womensConvertedShoeSizeLabel.text = "\(womensConvertedSize) in European Shoe Size"
+        
+        
+    }
 
 }
 
